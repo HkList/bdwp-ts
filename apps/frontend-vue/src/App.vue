638a84b2994ba76afcafb033ec53a1293ed0e379
huankong233 <huank0ng@foxmail.com>
2026-02-26 10:57:21 +0800

    [*] 优化首屏动画¿

--- apps/frontend-vue/src/App.vue
+++ apps/frontend-vue/src/App.vue
@@ -2,7 +2,21 @@
 import { proConfigProviderProps } from '@frontend/hooks/useTheme.ts'
 import { NGlobalStyle } from 'naive-ui'
 import { ProConfigProvider } from 'pro-naive-ui'
+import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
+
+onMounted(() => {
+  requestAnimationFrame(() => {
+    const loadingElement = document.getElementById('app-loading')
+    if (loadingElement) {
+      loadingElement.classList.add('loaded')
+      // 动画结束后移除元素
+      setTimeout(() => {
+        loadingElement.remove()
+      }, 300)
+    }
+  })
+})
 </script>
 
 <template>
--- apps/frontend-vue/src/main.ts
+++ apps/frontend-vue/src/main.ts
@@ -11,15 +11,3 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
-
-// 应用挂载完成后隐藏加载动画
-requestAnimationFrame(() => {
-  const loadingElement = document.getElementById('app-loading')
-  if (loadingElement) {
-    loadingElement.classList.add('loaded')
-    // 动画结束后移除元素
-    setTimeout(() => {
-      loadingElement.remove()
-    }, 300)
-  }
-})
